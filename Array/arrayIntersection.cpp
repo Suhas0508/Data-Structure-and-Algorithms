d08da0ce228e8eafe624ce6ec4ba920c2ad16454
Suhas0508 <96803394+Suhas0508@users.noreply.github.com>
2022-06-11 20:21:41 +0530

    Update arrayIntersection.cpp

--- Array/arrayIntersection.cpp
+++ Array/arrayIntersection.cpp
@@ -1,1 +1,33 @@
 
+#include <iostream>
+using namespace std;
+
+void intersectinArray(int arr[],int n,int arr1[] ,int n1){
+
+    cout<<"{ ";
+	
+    for(int i=0; i<n; i++){
+		for(int j=0; j<n1; j++){
+			if(arr[i]==arr1[j]){
+				cout<<arr[i]<<",";
+			}
+		}
+		
+	}
+		
+    
+	   
+	cout<<" }"<<endl;
+}
+
+int main() {
+    cout << "Revese Array program"<<endl;
+    
+    int arr[5]={1,2,3,4,5};
+	int arr1[7]={5,2,7,6,8,9,4};
+	
+    
+	intersectinArray(arr,5,arr1,7);
+
+    return 0;
+}
