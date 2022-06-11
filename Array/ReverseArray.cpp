60e7095720745395f77cb12b37c2a6811c05237c
Suhas0508 <96803394+Suhas0508@users.noreply.github.com>
2022-06-11 19:42:17 +0530

    Update and rename ReverseArray to ReverseArray.cpp

--- Array/ReverseArray.cpp
+++ Array/ReverseArray.cpp
@@ -5,11 +5,11 @@ using namespace std;
 void reverseArray(int arr[], int n){
 
     cout<<"Reverse Array : { ";
-	int end=n;
+	int end=n-1;
 	int start=0;
     for(int i=0; i<n; i++){
 		
-		if(start!=end){
+		if(start<=end){
 			int temp=arr[start];
 			arr[start]=arr[end];
 			arr[end]=temp;
@@ -28,9 +28,12 @@ int main() {
     
     int arr[5]={1,2,3,4,5};
 	int arr1[7]={5,2,7,6,5,9,4};
+	
+	int arr2[13]={5,2,7,6,3,6,8,9,0,6,5,9,4};
     
     reverseArray(arr,5);
 	reverseArray(arr1,7);
+	reverseArray(arr2,13);
 
     return 0;
 }
