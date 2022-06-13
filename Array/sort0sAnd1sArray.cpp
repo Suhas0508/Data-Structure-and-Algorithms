b4ac7a25d84dfc8317d24d378609bdbb66d38a95
Suhas0508 <96803394+Suhas0508@users.noreply.github.com>
2022-06-13 16:24:45 +0530

    Update sort0sAnd1sArray.cpp

--- Array/sort0sAnd1sArray.cpp
+++ Array/sort0sAnd1sArray.cpp
@@ -1,23 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void sort(int arr[], int n){
-	
+void printArray(int arr[], int n){
 	for(int i=0; i<n; i++){
-		for(int j=i+1; j<n; j++){
-			if(arr[i]>arr[j]){
-				int temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-			}
-		}
 		cout<<arr[i]<<",";
 	}
+}
+
+void sort(int arr[], int n){
+	//-------------------------------------------------------------------
+	// for(int i=0; i<n; i++){
+	// 	for(int j=i+1; j<n; j++){
+	// 		if(arr[i]>arr[j]){
+	// 			int temp = arr[i];
+	// 			arr[i] = arr[j];
+	// 			arr[j] = temp;
+	// 		}
+	// 	}
+	// 	cout<<arr[i]<<",";
+	// }
 	
+	//-------------------------------------------------------------------
+	
+	//Using Optimal solution (BEST SOLUTION)
+	
+	int i=0, j=n-1;
+	while(i<j){
+		
+		while(arr[i] == 0 && i<j)
+			i++;
+		
+		while(arr[j] == 1 && i<j)
+			j--;
+		
+		if(arr[i]==1 && arr[j] == 0 && i<j){
+		
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+			i++;
+			j--;
+		}	
+	}
 }
  
 int main()
 {
     int arr[] = { 1, 0, 0, 1, 0, 1, 0, 0, 1, 1 };
    	sort(arr,10);
+	printArray(arr,10);
 }
