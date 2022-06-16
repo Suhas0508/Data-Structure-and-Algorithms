c52b2645b878721f2770e97cd9a35b8dafdd0064
Suhas0508 <96803394+Suhas0508@users.noreply.github.com>
2022-06-16 17:43:40 +0530

    Create separateDigit.cpp

---
+++ CPP-Programs/separateDigit.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+    int rev=0, n = 123;
+    
+    
+    while(n!=0){
+        int temp=0;
+        temp = n%10;
+      
+        cout<<temp<<endl;
+    
+        n = n/10;
+        
+    }
+}
