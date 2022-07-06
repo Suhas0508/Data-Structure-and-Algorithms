432b0aef62eea56497b81aa70fb67aedf3ac1bc5
Suhas Tippe <96803394+Suhas0508@users.noreply.github.com>
2022-07-06 19:28:55 +0530

    Create insertionSort.cpp

---
+++ Array/insertionSort.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+void insertionSort(int arr[], int n){
+
+    for(int i=1; i<n; i++){
+        int temp = arr[i];
+        int j = i-1;
+        for(; j>=0; j--){
+           if(arr[j] > temp){
+               arr[j+1] = arr[j];
+           }else{
+               break;
+           }
+        }
+        arr[j+1] = temp;
+    }    
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<",";
+    }
+}
+
+int main()
+{
+    int arr[6] = {2,5,4,1,3,1};
+    insertionSort(arr, 6);
+    return 0;
+}
